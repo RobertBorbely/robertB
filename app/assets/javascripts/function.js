470db3c9cbf644b73bdc505d8e0d67e4d1d156d7
Borbély Róbert <respectsas@gmail.com>
2015-12-08 20:14:05 +0100

    Added mobile menu js

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -1,16 +1,23 @@
-(function($){
-  var $mobileNavToggleBtn = $('.mobile-nav-toggle');
+(function ($) {
 
-  function onBtnClick(e){
-    var $this = $(this),
-        $selectors = $('.mobile-nav');
+    "use strict"
 
-    $this.toggleClass('is-open');    
-    $selectors.toggleClass('is-open');
-  }
+    function onMobileToggleClick (e) {
+        var $this = $(this),
+            $navigation = $('.mobile-nav');
 
-  $(document).ready(function(){
-    $mobileNavToggleBtn.on('click', onBtnClick);
-  });
+        $this.toggleClass('is-open');
+        $navigation.toggleClass('is-open');
+    }
+
+    function bindings () {
+        // Toggle mobile navigation (mobileNav)
+        $('.mobile-nav-toggle').on('click', onMobileToggleClick);
+
+    }
+
+    $(document).ready(function () {
+        bindings();
+    });
 
 })(jQuery);
